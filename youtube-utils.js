dd393d299237b744e89a26fa3e68635193bb7d1e
Ylmaz2006 <drademademogullari@gmail.com>
2025-08-27 01:45:13 +0300

    Your commit message

---
+++ youtube-utils.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY; // Store your key in env!
+
+async function searchYouTubeVideos(query, maxResults = 5) {
+  const url = `https://www.googleapis.com/youtube/v3/search`;
+  const params = {
+    part: 'snippet',
+    q: query,
+    type: 'video',
+    maxResults,
+    key: YOUTUBE_API_KEY
+  };
+
+  try {
+    const response = await axios.get(url, { params });
+    return response.data.items.map(item => ({
+      title: item.snippet.title,
+      videoId: item.id.videoId,
+      url: `https://www.youtube.com/watch?v=${item.id.videoId}`,
+      thumbnail: item.snippet.thumbnails.default.url,
+      description: item.snippet.description
+    }));
+  } catch (error) {
+    console.error('YouTube Search Error:', error.message);
+    return [];
+  }
+}
+
+module.exports = { searchYouTubeVideos };
