882f2aa8ce622722d418a10fd8ea8a1a13a9145b
Ylmaz2006 <drademademogullari@gmail.com>
2025-08-27 18:08:43 +0300

    Your commit message

--- youtube-utils.js
+++ youtube-utils.js
@@ -1,13 +1,7 @@
 const axios = require('axios');
 
-const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY; // Store your key in env!
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
-/**
- * Search YouTube for videos by relevance (most relevant first).
- * @param {string} query - Search query, e.g., "artist - song".
- * @param {number} maxResults - Number of results to return (default 7).
- * @returns {Promise<Array>} Array of video objects (title, videoId, url, thumbnail, description).
- */
 async function searchYouTubeVideos(query, maxResults = 7) {
   const url = `https://www.googleapis.com/youtube/v3/search`;
   const params = {
@@ -15,7 +9,8 @@ async function searchYouTubeVideos(query, maxResults = 7) {
     q: query,
     type: 'video',
     maxResults,
-    order: 'relevance',    // <--- This gets videos by relevance, not view count!
+    order: 'relevance',         // Most relevant videos
+    videoDuration: 'medium',    // Only videos between 4 and 20 mins
     key: YOUTUBE_API_KEY
   };
 
@@ -34,38 +29,4 @@ async function searchYouTubeVideos(query, maxResults = 7) {
   }
 }
 
-async function recognizeYouTubeMusic(youtubeUrl) {
-  const acrHost = process.env.ACRCLOUD_HOST;
-  const acrKey = process.env.ACRCLOUD_ACCESS_KEY;
-  const acrSecret = process.env.ACRCLOUD_ACCESS_SECRET;
-
-  const endpoint = `https://${acrHost}/v1/identify`;
-
-  // You need to generate a signature for AcrCloud
-  const crypto = require('crypto');
-  const timestamp = Math.floor(Date.now() / 1000);
-  const stringToSign = ['POST', endpoint, acrKey, 'audio', timestamp].join('\n');
-  const signature = crypto.createHmac('sha1', acrSecret).update(stringToSign).digest('base64');
-
-  const data = {
-    url: youtubeUrl
-  };
-
-  try {
-    const response = await axios.post(endpoint, data, {
-      headers: {
-        'access-key': acrKey,
-        'signature': signature,
-        'timestamp': timestamp,
-        'Content-Type': 'application/json'
-      }
-    });
-    console.log('AcrCloud Output:', JSON.stringify(response.data, null, 2));
-    return response.data;
-  } catch (err) {
-    console.error('AcrCloud Error:', err.response?.data || err.message);
-    return null;
-  }
-}
-
-module.exports = { searchYouTubeVideos, recognizeYouTubeMusic };
+module.exports = { searchYouTubeVideos };
